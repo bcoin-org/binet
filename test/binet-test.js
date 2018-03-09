15a2015146bc37ad2f4097ad5797779488cd117e
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-03-09 10:55:32 -0800

    test: fix style.

--- test/binet-test.js
+++ test/binet-test.js
@@ -1,37 +1,44 @@
-'use strict'
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
 
-const assert = require('./util/assert')
-const binet  = require('../lib/binet')
+'use strict';
 
-describe('Binet', function(){
-    it('should convert binary addresses to string addresses', () => {
-        const validOnionAddress = Buffer.from('fd87d87eeb43ffffffffffffffffffff', 'hex');
-        const invalidOnionAddress = Buffer.from('fd87d87eeb43fffffffffffffffffff', 'hex');
+const assert = require('./util/assert');
+const binet  = require('../lib/binet');
 
-        assert(binet.toString(validOnionAddress), 'Invalid onion address');
-        try {
-            assert(binet.toString(invalidOnionAddress), 'Invalid onion address mistakenly interpreted as valid');
-        } catch(e) {
-            if(e.message !== 'Invalid IP address.'){
-                throw e;
-            }
-        }
-    });
+describe('binet', function() {
+  it('should convert binary addresses to string addresses', () => {
+    const validOnionAddress = Buffer.from(
+      'fd87d87eeb43ffffffffffffffffffff', 'hex');
+    const invalidOnionAddress = Buffer.from(
+      'fd87d87eeb43fffffffffffffffffff', 'hex');
 
-    it('convert a buffer into an ip address string', () => {
-        const validIpV4Buffer = Buffer.from('c0a80101', 'hex');
-        const invalidIpV4Buffer = Buffer.from('fffa8010111', 'hex');
-        const validIpV6Buffer = Buffer.from('20010db885a3000000008a2e03707334', 'hex');
-        const invalidIpV6Buffer = Buffer.from('', 'hex');
+    assert(binet.toString(validOnionAddress), 'Invalid onion address');
 
-        assert(binet.encode(validIpV4Buffer) === '192.168.1.1');
-        assert(binet.encode(validIpV6Buffer) === '2001:db8:85a3::8a2e:370:7334');
-        try {
-            assert(binet.encode(invalidIpV4Buffer), 'Invalid IPv4 buffer interpreted as valid buffer');
-        } catch(e) {
-            if(e.message !== 'Invalid IP address.'){
-                throw e;
-            }
-        }
-    })
-})
+    try {
+      assert(binet.toString(invalidOnionAddress),
+        'Invalid onion address mistakenly interpreted as valid');
+    } catch (e) {
+      if (e.message !== 'Invalid IP address.')
+        throw e;
+    }
+  });
+
+  it('convert a buffer into an ip address string', () => {
+    const validIpV4Buffer = Buffer.from('c0a80101', 'hex');
+    const invalidIpV4Buffer = Buffer.from('fffa8010111', 'hex');
+    const validIpV6Buffer = Buffer.from(
+      '20010db885a3000000008a2e03707334', 'hex');
+
+    assert(binet.encode(validIpV4Buffer) === '192.168.1.1');
+    assert(binet.encode(validIpV6Buffer) === '2001:db8:85a3::8a2e:370:7334');
+
+    try {
+      assert(binet.encode(invalidIpV4Buffer),
+        'Invalid IPv4 buffer interpreted as valid buffer');
+    } catch (e) {
+      if (e.message !== 'Invalid IP address.')
+        throw e;
+    }
+  });
+});
